3c8e348f9c3a36af478936c9206c6443bf6a54cc
hamzzi0097 <81970551+hamzzi0097@users.noreply.github.com>
2025-02-28 18:44:54 +0900

    중간 커밋

---
+++ Assets/Script/Kickboard.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine;
+
+public class Kickboard : UdonSharpBehaviour
+{
+    public override void OnPickup()
+    {
+        // 플레이어가 오브젝트를 잡기 시작할 때 실행
+        Debug.Log("오브젝트가 잡혔습니다.");
+    }
+
+    public override void OnDrop()
+    {
+        // 플레이어가 오브젝트를 놓았을 때 실행
+        Debug.Log("오브젝트가 놓였습니다.");
+    }
+}
