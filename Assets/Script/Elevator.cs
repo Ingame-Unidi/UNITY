0ff01c5dfcda4150bacfd8e7fa20fea55d8418c6
hamzzi0097 <act0097@gmail.com>
2025-04-02 02:50:53 +0900

    엘레베이터 사운드 제작
    - 엘레베이터 사운드 플레이 및 버튼 클릭 시 엔딩 사운드 플레이

---
+++ Assets/Script/Elevator.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Elevator : UdonSharpBehaviour
+{
+    public AudioSource elevatorSound;
+    public AudioSource endingSound;
+
+    public override void Interact()
+    {
+        Debug.Log("Game End");
+
+        // 기존 음악 중지 및 엔딩 음악 플레이
+        elevatorSound.Stop();
+        endingSound.Play();
+    }
+}
